6b5b17c52ac56d376f9601d6a1fafc1272f93329
Vidushi Tiwari <128062856+vidsht@users.noreply.github.com>
2024-10-16 23:38:25 +0530

    More work on responsiveness

--- script.js
+++ script.js
@@ -37,8 +37,18 @@ var swiper = new Swiper(".mySwiper", {
 page4Animation()
 swiperAnimation()
 
-var menu = document.querySelector("nav h3")
+var menu = document.querySelector("#nav h3")
 var full = document.querySelector("#full-scr")
+var navimg = document.querySelector("#nav img")
+var flag = 0
 menu.addEventListener("click", function() {
-    full.style.top = 0
+    if (flag == 0){
+        full.style.top = 0
+        navimg.style.opacity = 0
+        flag = 1
+    } else{
+        full.style.top = "-100%"
+        navimg.style.opacity = 1
+        flag = 0
+    }
 })
