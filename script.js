cd36afdf4da6c272172b086c9fcefd0fbabf3bee
Vidushi Tiwari <128062856+vidsht@users.noreply.github.com>
2024-10-18 01:03:37 +0530

    Added Loader

--- script.js
+++ script.js
@@ -33,22 +33,31 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
 }
+function menuAnimation() {
+    var menu = document.querySelector("#nav h3")
+    var full = document.querySelector("#full-scr")
+    var navimg = document.querySelector("#nav img")
+    var flag = 0
+    menu.addEventListener("click", function() {
+        if (flag == 0){
+            full.style.top = 0
+            navimg.style.opacity = 0
+            flag = 1
+        } else{
+            full.style.top = "-100%"
+            navimg.style.opacity = 1
+            flag = 0
+        }
+    })
+}
+function loaderAnimation() {
+    var loader = document.querySelector("#loader")
+    setTimeout(function() {
+        loader.style.top = "-100%"
+},4200)
+}
 
 page4Animation()
 swiperAnimation()
-
-var menu = document.querySelector("#nav h3")
-var full = document.querySelector("#full-scr")
-var navimg = document.querySelector("#nav img")
-var flag = 0
-menu.addEventListener("click", function() {
-    if (flag == 0){
-        full.style.top = 0
-        navimg.style.opacity = 0
-        flag = 1
-    } else{
-        full.style.top = "-100%"
-        navimg.style.opacity = 1
-        flag = 0
-    }
-})
+menuAnimation()
+loaderAnimation()
